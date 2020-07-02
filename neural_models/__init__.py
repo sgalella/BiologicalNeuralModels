0ae6aca4f3fa986bbb0e37f4f4cdafee61dace00
sgalella <santi.galella@gmail.com>
2020-07-02 20:46:29 +0200

    Update imports

---
+++ neural_models/__init__.py
@@ -0,0 +1,6 @@
+from .fitzhugh_nagumo import FitzHughNagumo
+from .hindmarsh_rose import HindmarshRose
+from .hodgkin_huxley import HodgkinHuxley
+from .leaky_integrate_and_fire import LeakyIntegrateAndFire
+from .morris_lecar import MorrisLecar
+from .rulkov_map import RulkovMap
